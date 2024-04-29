47cc3e72181263c2fe2bc6889b32dc1b45deba7d
Andy <andy.tisba+dev@gmail.com>
2024-04-29 10:49:37 +0200

    first commit

---
+++ assets/switch-button.js
@@ -0,0 +1,39 @@
+// <switchButton></switchButton>
+class SwitchButton extends HTMLElement {
+  constructor() {
+    super();
+
+    this.addEventListener("click", this.onClick.bind(this));
+    this.input = this.querySelector("input");
+    this.cardImages = document.querySelectorAll(".card__inner img");
+    this.imageWidths = ["165w", "360w", "533w", "720w", "940w", "1000w"];
+  }
+
+  toggle() {
+    this.input.checked = !this.input.checked;
+  }
+
+  onClick(event) {
+    event.preventDefault();
+    this.toggle();
+    this.cardImages.forEach((image) => {
+      const newSrc = image.dataset[this.input.checked ? "src-1" : "src-2"];
+      const newSrcSet = this.imageWidths
+        .map((width) => `${newSrc} ${width}`)
+        .join(", ");
+      image.src = `${newSrc} 533w`;
+      image.srcset = newSrcSet;
+    });
+  }
+
+  connectedCallback() {}
+
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    console.log("component changed");
+  }
+  disconnectedCallback() {
+    console.log("component unmounted");
+  }
+}
+
+customElements.define("switch-button", SwitchButton);
